68a28d883b8bcc38f056741c03ea4406a8b2ac78
Sujith S <digitizedx@gmail.com>
2021-10-10 13:06:41 +0530

    Secrets bundle added

--- app/src/main/java/com/ss/cryptobox/SecretsViewActivity.java
+++ app/src/main/java/com/ss/cryptobox/SecretsViewActivity.java
@@ -1,12 +1,18 @@
 package com.ss.cryptobox;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.savedstate.SavedStateRegistryOwner;
 
 public class SecretsViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.secrets_view);
+        Intent intent = getIntent();
+        Bundle b = intent.getExtras();
+        String s = b.getString("secrets");
+        if(s.equals("message"))
+            setContentView(R.layout.secrets_view);
     }
 }
