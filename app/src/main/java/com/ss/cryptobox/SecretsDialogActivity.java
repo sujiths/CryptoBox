ec8806d144cc373212bf4232d40002458dfd7709
Sujith S <digitizedx@gmail.com>
2021-10-15 20:50:25 +0530

    Added dialog activity

---
+++ app/src/main/java/com/ss/cryptobox/SecretsDialogActivity.java
@@ -0,0 +1,11 @@
+package com.ss.cryptobox;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecretsDialogActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_user_pass);
+    }
+}
--- app/src/main/java/com/ss/cryptobox/SecretsViewActivity.java
+++ app/src/main/java/com/ss/cryptobox/SecretsViewActivity.java
@@ -2,7 +2,11 @@ package com.ss.cryptobox;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.savedstate.SavedStateRegistryOwner;
 
 public class SecretsViewActivity extends AppCompatActivity {
@@ -14,5 +18,12 @@ public class SecretsViewActivity extends AppCompatActivity {
         String s = b.getString("secrets");
         if(s.equals("message"))
             setContentView(R.layout.secrets_view);
+
+        Button biometricLoginButton = findViewById(R.id.add_item);
+        biometricLoginButton.setOnClickListener(view -> {
+            startActivity(new Intent(getApplicationContext(), SecretsDialogActivity.class));
+//            DialogFragment newFragment = new AddSecretDataFragment();
+//            newFragment.show(getSupportFragmentManager(), "user_pass");
+        });
     }
 }
