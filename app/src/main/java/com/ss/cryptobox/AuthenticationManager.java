e63e38d0b174cea2cf42f8e33d064bdffe3ae306
Sujith S <digitizedx@gmail.com>
2021-10-16 23:41:39 +0530

    Added check for availability of Biometric

--- app/src/main/java/com/ss/cryptobox/AuthenticationManager.java
+++ app/src/main/java/com/ss/cryptobox/AuthenticationManager.java
@@ -1,7 +1,16 @@
 package com.ss.cryptobox;
 
+import static android.hardware.biometrics.BiometricManager.Authenticators.BIOMETRIC_STRONG;
+import static android.hardware.biometrics.BiometricManager.Authenticators.DEVICE_CREDENTIAL;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.biometric.BiometricManager;
 import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
 
@@ -20,7 +29,11 @@ public class AuthenticationManager {
     }
 
 
-    public void Initialise(AppCompatActivity activity, IAuthListener authListener) {
+    public boolean Initialise(AppCompatActivity activity, IAuthListener authListener) {
+
+        if(!isBiometricAuthAvailable(activity))
+            return false;
+
         executor = ContextCompat.getMainExecutor(activity);
         biometricPrompt = new BiometricPrompt(activity,
                 executor, new BiometricPrompt.AuthenticationCallback() {
@@ -42,15 +55,38 @@ public class AuthenticationManager {
                 super.onAuthenticationFailed();
             }
         });
+        return true;
     }
 
-    public void requestAuthentication() {
-        promptInfo = new BiometricPrompt.PromptInfo.Builder()
-                .setTitle("Biometric login for my app")
-                .setSubtitle("Log in using your biometric credential")
-                .setNegativeButtonText("Use account password")
-                .build();
-        biometricPrompt.authenticate(promptInfo);
+    private boolean isBiometricAuthAvailable(Context context) {
+        boolean result = false;
+        BiometricManager biometricManager = BiometricManager.from(context);
+        switch (biometricManager.canAuthenticate()) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                Log.i(TAG, "App can authenticate using biometrics.");
+                result = true;
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Log.e(TAG, "No biometric features available on this device.");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Log.e(TAG, "Biometric features are currently unavailable.");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Log.e(TAG, "Not enrolled for biometric");
+                break;
+        }
+        return result;
+    }
+    public void RequestAuthentication(Context context) {
+        if(isBiometricAuthAvailable(context)) {
+            promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("Biometric login for my app")
+                    .setSubtitle("Log in using your biometric credential")
+                    .setNegativeButtonText("Use account password")
+                    .build();
+            biometricPrompt.authenticate(promptInfo);
+        }
     }
 
 
